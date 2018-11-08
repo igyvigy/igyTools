d2f89585c61bd1d7fd84b189989f4bb5023f09ce
Andrii Narinian <andrii.narinian@rolique.io>
2018-11-08 20:43:23 +0200

    Initial Commit

---
+++ igyTools/igyTools.h
@@ -0,0 +1,19 @@
+//
+//  igyTools.h
+//  igyTools
+//
+//  Created by Andrii on 11/8/18.
+//  Copyright © 2018 rolique. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for igyTools.
+FOUNDATION_EXPORT double igyToolsVersionNumber;
+
+//! Project version string for igyTools.
+FOUNDATION_EXPORT const unsigned char igyToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <igyTools/PublicHeader.h>
+
+
