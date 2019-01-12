dec1830c6421eab7511672fb2c1af8c68a134391
bbb <sssbohdan@gmail.com>
2019-01-12 21:17:00 +0200

    Added files to project

--- igyTools/Source/Extensions/UICollectionView.swift
+++ igyTools/Source/Extensions/UICollectionView.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2018 rolique. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 public extension UICollectionView {
   public func setDelegateAndDataSource(_ object: UICollectionViewDelegate & UICollectionViewDataSource) {
