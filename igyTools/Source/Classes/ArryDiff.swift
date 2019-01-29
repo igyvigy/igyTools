2291f6eeb17773ceb925b13881b5cfc17875e2c5
bbb <sssbohdan@gmail.com>
2019-01-29 14:03:02 +0200

    Small refactoring.

--- igyTools/Source/Classes/ArryDiff.swift
+++ igyTools/Source/Classes/ArryDiff.swift
@@ -3,8 +3,8 @@ import Foundation
 
 public func arrayDiff<T1, T2>(_ first: [T1], _ second: [T2], with compare: (T1, T2) -> Bool) -> ArryDiff<T1, T2> {
   let combinations = first.compactMap { firstElement in (firstElement, second.first { secondElement in compare(firstElement, secondElement) }) }
-  let common = combinations.filter { $0.1 != nil }.compactMap { ($0.0, $0.1!) }
-  let removed = combinations.filter { $0.1 == nil }.compactMap { ($0.0) }
+  let common = combinations.filter { $0.1 != nil }.map { ($0.0, $0.1!) }
+  let removed = combinations.filter { $0.1 == nil }.map { ($0.0) }
   let inserted = second.filter { secondElement in !common.contains { compare($0.0, secondElement) } }
   
   return ArryDiff(common: common, removed: removed, inserted: inserted)
